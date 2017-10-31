0431cccedfce48617c4ba968dba11d939088b9a3
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-30 20:05:37 -0400

    added  things for inputing an artist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      events: []
+    };
+  }
+
+  handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("submitted", evt, this.state);
+  }
+
+
+  handleArtChange = (evt) => {
+    console.log("text changed!", evt.target.value, this.state);
+    var needle = evt.target.value;
+    this.setState(() => {
+      return {
+        needle
+      }
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React & APIs!!!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <header>
+            <form onSubmit={evt => this.handleFormSubmit(evt)}>
+              <input
+                type="text"
+                placeholder="search for an artist"
+                onChange={evt => this.handleArtChange(evt)}/>
+            </form>
+          </header>
+        </div>
       </div>
     );
   }
