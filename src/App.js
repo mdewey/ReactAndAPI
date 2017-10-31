03527a8fe07c1d84524d9f72ca823a11bf2e7386
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-30 21:21:43 -0400

    listing concerts and dates

--- src/App.js
+++ src/App.js
@@ -2,32 +2,48 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Moment from 'react-moment';
+import 'moment-timezone';
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      events: []
+      events: [],
+      musicAPIKey: ""
     };
   }
 
   handleFormSubmit = (evt) => {
     evt.preventDefault();
     console.log("submitted", evt, this.state);
+    fetch(`http://api.jambase.com/artists?name=${this.state.needle}&page=0&api_key=mzrnmarv3wp68ch8wr553bac`)
+      .then(resp => resp.json())
+      .then(json => {
+        console.log("back", json)
+        if (json.Artists.length) {
+          const artistId = json.Artists[0].Id
+          console.log("searching for artist", artistId);
+          return fetch(`http://api.jambase.com/events?artistId=${artistId}&page=0&api_key=mzrnmarv3wp68ch8wr553bac`)
+        }
+      })
+      .then(resp => resp.json())
+      .then(json => {
+        console.log("back again", json)
+        this.setState(() => {
+          return {events: json.Events}
+        })
+      });
   }
-
 
   handleArtChange = (evt) => {
     console.log("text changed!", evt.target.value, this.state);
     var needle = evt.target.value;
     this.setState(() => {
-      return {
-        needle
-      }
+      return {needle}
     })
   }
-
-
 
   render() {
     return (
@@ -36,15 +52,28 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React & APIs!!!</h1>
         </header>
+        <br/>
         <div className="App-intro">
           <header>
             <form onSubmit={evt => this.handleFormSubmit(evt)}>
               <input
                 type="text"
                 placeholder="search for an artist"
+                autoFocus="autoFocus"
                 onChange={evt => this.handleArtChange(evt)}/>
             </form>
           </header>
+          <section>
+            {this.state.events.map((e, i) => {
+              return (
+                <div className="event" key={i}>
+                  {e.Venue.Name} @ 
+                  <Moment format="M/D/YY">{e.Date}</Moment>
+                  
+                </div>
+              )
+            })}
+          </section>
         </div>
       </div>
     );
