93f2cc1b52c3e90379c1d35bdde1cf485d0b8945
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-31 10:07:20 -0400

    added html for styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import 'moment-timezone';
 import Weather from './Components/weather'
 import api from './Components/music-api'
 
-
 class App extends Component {
 
   constructor() {
@@ -61,10 +60,22 @@ class App extends Component {
               .map((e, i) => {
                 return (
                   <div className="event" key={i}>
-                    {e.Venue.Name}
-                    @
-                    <Moment format="M/D/YY">{e.Date}</Moment>
-                    <Weather latitude={e.Venue.Latitude} longitude={e.Venue.Longitude}/>
+                    <div>
+                      <Moment format="MMM">{e.Date}</Moment>
+                      <Moment format="Do">{e.Date}</Moment>
+                      <Moment format="YYYY">{e.Date}</Moment>
+                    </div>
+                    <div>
+                      <div>
+                        Venue: {e.Venue.Name}
+                      </div>
+                      <div>
+                        City: {e.Venue.City}, {e.Venue.State}, {e.Venue.StateCode}
+                      </div>
+                    </div>
+                    <div>
+                      <Weather latitude={e.Venue.Latitude} longitude={e.Venue.Longitude}/>
+                    </div>
                   </div>
                 )
               })}
