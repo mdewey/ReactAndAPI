e6a01b7b580b388cf5cf8cc916c5fd95f0879583
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-31 11:37:22 -0400

    styled and added temp to the data

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -5,10 +5,10 @@ class Weather extends React.Component {
     constructor(props) {
         super(props);
         console.log(['weather', 'ctor', props])
-        
+
         this.state = {
-            location:{
-                lat:props.latitude,
+            location: {
+                lat: props.latitude,
                 lng: props.longitude
             }
         };
@@ -17,25 +17,26 @@ class Weather extends React.Component {
     componentDidMount = () => {
         console.log(['weather', 'mount', this.state])
         fetch(`http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${this.state.location.lat}&lon=${this.state.location.lng}&appid=3061809775bb7491bd85e0a46e15e0d1`)
-        .then(resp => resp.json())
-        .then(json => {
-            console.log("got the weather", json)
-            this.setState(() => {
-                return {
-                    weather:json
-                }
+            .then(resp => resp.json())
+            .then(json => {
+                console.log("got the weather", json)
+                this.setState(() => {
+                    return {weather: json}
+                })
             })
-        })
     }
 
     render() {
         console.log(['weather', 'render', this.state])
         if (this.state.weather) {
             return (
-                <div>
-                   Current Weather:
-                   {this.state.weather.weather[0].main}
+                <section>
+                    <div>
+                    <i className={"owf owf-"+ this.state.weather.weather[0].id}></i>
+                    <span className="temp">{this.state.weather.main.temp} &#176; </span>
                     </div>
+                    <em>Current Weather</em>
+                </section>
             )
         } else {
             return <div className="col-md-6">
